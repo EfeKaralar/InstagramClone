93ad91769081195fd9ef22521e1134ddced9839a
EfeKaralar <alpefeka2001@gmail.com>
2021-03-17 01:22:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,25 @@
+package com.example.instagramclone;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("GAWeEfnfpdEsdyJ5zwFORDU955Kp2lIAhGnuwEw9")
+                .clientKey("tfNsipkWFpKhYdcRqw17OIrjwf07GVFU9GJR4KG5")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
